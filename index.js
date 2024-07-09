e0ad2688a82bc6d124b3a347359fc8471492800a
andresg <ad.garciagarcia04@gmail.com>
2024-07-08 23:27:07 -0500

    adicion de la funcionalidad para desencriptar un texto

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ function encriptar(){
     let logo = document.getElementById("logo");
 
     let textoCifrado = texto
-                        .replace(/a/gi, "ai")
+
                         .replace(/e/gi,"enter")
                         .replace(/i/gi, "imes")
+                        .replace(/a/gi, "ai")
                         .replace(/o/gi, "ober")
                         .replace(/u/gi, "ufat");
     if (texto.length != 0){
@@ -23,5 +24,33 @@ function encriptar(){
         alert ("Debes ingresar algun texto ") ;
     }
 
+
+}
+
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let logo = document.getElementById("logo");
+
+    let textoCifrado = texto
+
+                        .replace(/enter/gi,"e")
+                        .replace(/imes/gi, "i")
+                        .replace(/ai/gi, "a")
+                        .replace(/ober/gi, "o")
+                        .replace(/ufat/gi, "u");
+    
+    if (texto.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con exito";
+        parrafo.textContent = "";
+        logo.src = "./imagenes/3_imagen.jpg";
+    } else{
+        logo.scr = "./imagenes/1_imagen.jpg";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa al texto que deseas encriptar o desecriptar";
+        alert ("Debes ingresar algun texto ") ;
+    }
 
 }
